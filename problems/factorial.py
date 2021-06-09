f29c0c26cf22677d7b857565991b3d7a70183cb7
Vishal Tanwar <vishal.tanwar@outlook.com>
2021-06-09 16:00:38 +0530

    Added space and time complexity to factorial

--- problems/factorial.py
+++ problems/factorial.py
@@ -1,10 +1,14 @@
 def factorial(number: int) -> int:
-    """Recursively calculates the factorial of a number"""
+    """
+    Recursively calculates the factorial of a number
+    Time complexity is O(n) and Space complexity is also O(n) as the stack requires function calls
+    equal to n at the same time.
+    """
 
     if number < 0:
         raise ValueError('Factorial cannot be calculated for negative numbers')
 
-    if number == 0:
+    if number == 0 or number == 1:
         return 1
 
     return number * factorial(number - 1)
