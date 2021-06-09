25b3a775a65d2e465329a57ecfa181f04ed30ecd
Vishal Tanwar <vishal.tanwar@outlook.com>
2021-06-09 15:37:24 +0530

    Added time & space comlexity notes

--- problems/armstrong.py
+++ problems/armstrong.py
@@ -13,6 +13,10 @@ def is_armstrong(number: int) -> bool:
     Checks whether a number is armstrong or not.
     An armstrong number is a number which satisfies this equation:
     abc = a^order(abc) + b^order(abc) + c^order(abc)
+    The time complexity for calculating the armstrong number is O(log n) since we are dividing the
+    number x amount of times to size it down to 1 and hence log n is the time complexity.
+    and the space complexity is O(1) as extra space is not required if the number of digits
+    scale up and down, the amount of space remains constant.
     """
 
     order = calculate_order(number)
