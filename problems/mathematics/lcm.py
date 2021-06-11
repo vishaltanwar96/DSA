8f8ae01e65446b13be5a34e329cc061a83c3a0b4
Vishal Tanwar <vishal.tanwar@outlook.com>
2021-06-11 14:56:39 +0530

    added lcm for multiple numbers function

--- problems/mathematics/lcm.py
+++ problems/mathematics/lcm.py
@@ -30,3 +30,18 @@ def lcm_using_gcd(num1: int, num2: int) -> int:
     """LCM * GCD = Number 1 * Number 2"""
 
     return (num1 * num2) // euclidean_gcd(num1, num2)
+
+
+def lcm_multiple_numbers(*numbers):
+    """
+    LCM of a number with itself is equal to the number since the 'least common multiple' of the number is the
+    number itself and we can start to build our solution based on that.
+    """
+
+    if not numbers:
+        return 1
+
+    result = numbers[0]
+    for number in numbers:
+        result = lcm(result, number)
+    return result
