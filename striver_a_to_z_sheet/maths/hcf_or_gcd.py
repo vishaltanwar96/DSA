6877cbe3323277a06346e29888ddab414f0aa2b8
Vishal Tanwar <vishal.tanwar@outlook.com>
2024-09-07 16:18:04 +0530

    Add optimised approach for hcf

--- striver_a_to_z_sheet/maths/hcf_or_gcd.py
+++ striver_a_to_z_sheet/maths/hcf_or_gcd.py
@@ -4,3 +4,13 @@ def hcf_brute_force(number1, number2) -> int:
         if number1 % number == 0 and number2 % number == 0:
             hcf = number
     return hcf
+
+
+def hcf_optimised(number1: int, number2: int) -> int:
+    if number1 == 0:
+        return number2
+    if number2 == 0:
+        return number1
+    if number1 > number2:
+        return hcf_optimised(number1 % number2, number2)
+    return hcf_optimised(number2 % number1, number1)
